85192981ad05e7b16934d0a32cc7b30e26943664
pedroaaluz <pedroaluz137@gmail.com>
2024-10-19 15:23:06 -0300

    Refactor product page: Remove unnecessary imports and components

--- src/app/(system)/@pages/products/[id]/edit/page.tsx
+++ src/app/(system)/@pages/products/[id]/edit/page.tsx
@@ -1,42 +1,6 @@
 "use client";
-import {
-  Card,
-  CardContent,
-  Grid,
-  Typography,
-  useMediaQuery,
-  useTheme,
-  CardMedia,
-  ListItem,
-  List,
-  ListItemAvatar,
-  Avatar,
-  ListItemText,
-  Skeleton,
-  SelectChangeEvent,
-  Box,
-  ListItemButton,
-  Button,
-} from "@mui/material";
-import {
-  LocalOffer as LocalOfferIcon,
-  Person as PersonIcon,
-  Inventory as InventoryIcon,
-  AttachMoney as AttachMoneyIcon,
-  LocationOn as LocationOnIcon,
-  Campaign as CampaignIcon,
-  AccessTime as AccessTimeIcon,
-  Edit as EditIcon,
-} from "@mui/icons-material";
-import { useParams, useRouter } from "next/navigation";
-import { useQuery } from "@tanstack/react-query";
-import type { IGetProductOutput } from "../../../../../types/products";
-import type { IListMovementOutput } from "../../../../../types/movements";
-import ResponsiveTable from "@/components/responsiveTable";
-import HeaderSearchBar from "@/components/tablePage/header/headerSearchBar";
-import { useState } from "react";
-import { useUser } from "@clerk/nextjs";
-import { handleQueryParams } from "@/utils/handleQueryParams";
+import { Typography, Box } from "@mui/material";
+import { useParams } from "next/navigation";
 
 export default function ProductPage() {
   const { id } = useParams<{ id: string }>();
