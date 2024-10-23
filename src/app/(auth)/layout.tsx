5cb03072fd3d9f619ace2bf6450d6d60a42ffc2e
pedroaaluz <pedroaluz137@gmail.com>
2024-10-22 21:36:53 -0300

    remove theme in main layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,8 @@
-"use client";
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
-import { useIsSmallScreen } from "@/hooks/isSmallScreen";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ClerkProvider>
-        <Toaster
-          position={useIsSmallScreen() ? "top-center" : "bottom-right"}
-        ></Toaster>
+        <Toaster expand={true} richColors closeButton></Toaster>
         <body className={inter.className}>{children}</body>
       </ClerkProvider>
     </html>
